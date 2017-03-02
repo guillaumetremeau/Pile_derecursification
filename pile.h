951f5923d9f10ed7082326bf52df7ffe51638415
BaptistePR <klevh1@gmail.com>
2017-03-02 11:06:53 +0100

    fin du developpement de pile.c/.h

--- pile.h
+++ pile.h
@@ -1,6 +1,8 @@
 #ifndef _PILE_PILEDEREC_TREMEAU_KLEVH_
 #define _PILE_PILEDEREC_TREMEAU_KLEVH_
 
+#include <stdlib.h>
+
 typedef int valeur_t;
 
 typedef struct
@@ -10,25 +12,76 @@ typedef struct
   int curseur;
 }pile_t;
 
-pile_t init_pile(int);
 
+
+/* ---------------------------------------------------
+ *
+ *              INIT_PILE
+ *
+ * Retourne une pile vide de taille maximal indique
+ * en parametre.
+ *
+ * Retour :
+ *  - tete de la pile initialisee
+ * --------------------------------------------------- */
+pile_t init_pile(unsigned);
+
+
+
+/* ---------------------------------------------------
+ *
+ *              FREE_PILE
+ *
+ * Libere la memoire allouee a la pile passee en
+ * parametre.
+ * --------------------------------------------------- */
 void free_pile(pile_t *);
 
+
+
+/* ---------------------------------------------------
+ *
+ *              EST_VIDE
+ *
+ * Indique si une pile passee en parametre est vide
+ * ou non.
+ *
+ * Retour :
+ *  - 1 si la pile est vide
+ *  - 0 sinon
+ * -------------------------------------------------- */
 int est_vide(pile_t);
 
-/* ----------------------------------------------
+
+
+/* ---------------------------------------------------
  *
  *              EMPILER
  *
- * Principe
- *
- * Lexique :
- *  - var : expl (entree/sortie)
+ * Empile la valeur passee en parametre dans la pile
+ * egalement passee en parametre si la pile n'est pas
+ * pleine.
  *
  * Retour :
- * ---------------------------------------------- */
+ *  - 0 si l'empilage a pu etre fait
+ *  - 1 sinon
+ * --------------------------------------------------- */
 int empiler(pile_t,valeur_t);
 
+
+
+/* ---------------------------------------------------
+ *
+ *              DEPILER
+ *
+ * Depile la premiere valeure de la pile passee en 
+ * parametre dans l'adresse egalement passee en
+ * parametre.
+ *
+ * Retour :
+ *  - 0 si le depilage a pu etre fait
+ *  - 1 sinon
+ * --------------------------------------------------- */
 int depiler(pile_t,valeur_t*);
 
 #endif
